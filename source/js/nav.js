9aa1c13059c046a341373eeb4e6610d397381e66
Vilenan <vilenan@mail.ru>
2023-05-25 16:42:03 +0300

    исправила открытие бургера

--- source/js/nav.js
+++ source/js/nav.js
@@ -5,8 +5,6 @@ const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
 console.log(headerHeight);
 
-document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
-
 burger.addEventListener('click', function(){
     burger.classList.toggle('burger--active');
     menu.classList.toggle('navigation--active');
